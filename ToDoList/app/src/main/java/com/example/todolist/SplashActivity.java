37b3019a363d619212d9562d7380e6035d96008e
Víctor Monzón <vi.monzon.perez@gmail.com>
2023-01-29 09:51:21 +0100

    final update + apk

--- ToDoList/app/src/main/java/com/example/todolist/SplashActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/SplashActivity.java
@@ -7,14 +7,17 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
     Timer timer;
-    Animation animlogo;
+    Animation animlogo, animtext;
     ImageView logo;
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,13 @@ public class SplashActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        animtext = AnimationUtils.loadAnimation(this,R.anim.anmationtext);
         animlogo = AnimationUtils.loadAnimation(this, R.anim.animationlogo);
         logo = (ImageView) findViewById(R.id.logo);
+        text = (TextView) findViewById(R.id.todolist);
 
         logo.startAnimation(animlogo);
-
+        text.startAnimation(animtext);
         timer = new Timer();
         timer.schedule(new TimerTask(){
             @Override
--- ToDoList/app/src/main/java/com/example/todolist/SplashActivity2.java
+++ ToDoList/app/src/main/java/com/example/todolist/SplashActivity2.java
@@ -8,19 +8,35 @@ import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity2 extends AppCompatActivity {
     Timer timer;
+    Animation animlogo, animtext;
+    ImageView logo;
+
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash2);
 
         getSupportActionBar().hide();
+
+        animlogo = AnimationUtils.loadAnimation(this,R.anim.animationlogo);
+        animtext = AnimationUtils.loadAnimation(this, R.anim.animbienvenido);
+
+        logo = (ImageView) findViewById(R.id.logosplash2);
+        text = (TextView) findViewById(R.id.bienvenido);
+
+        logo.startAnimation(animlogo);
+        text.startAnimation(animtext);
 
         timer = new Timer();
         timer.schedule(new TimerTask(){
